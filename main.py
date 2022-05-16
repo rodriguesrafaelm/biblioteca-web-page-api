5ea8efd6051a299b96288e52c17352504d45b9a7
rafaelmrodriguess <rafaelmrodrigues96@yahoo.com.br>
2022-05-15 22:54:59 -0300

    fixed problem with comments

--- main.py
+++ main.py
@@ -3,32 +3,9 @@ from flask_cors import CORS, cross_origin
 from controladores import *
 import os
 
-import json
-
 
 app = Flask(__name__)
 cors = CORS(app)
-
-'''@app.route("/livros", methods=['GET', 'POST', 'DELETE'])
-def livros():
-    if request.method == 'GET':
-        objetos = []
-        books_db = listar()
-        for livro in books_db:
-            a = {'id':livro[0], 'livro':livro[1], 'autor' : livro[2], 'ano_publicado' : livro[3], 'editora' : livro[4], 'image_url' : livro[5]}
-            objetos.append(a)
-        return jsonify(objetos)
-    if request.method == 'POST':
-        data = request.get_data().decode('utf-8')
-        data = json.loads(data)
-        add_livro(data['nome'], data['autor'])
-        return '200'
-    if request.method == 'DELETE':
-        data = request.get_data()
-        data = json.loads(data)
-        id = data['id_livro']
-        remover_livro(id)
-        return '200''''
 
 
 @app.route("/livros/<id>", methods=['GET'])
